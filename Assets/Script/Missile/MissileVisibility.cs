4fe5de643f1a52a3e479f1824fe68f06029bbc66
Ryoma Saito(Dedicated School Account) <96648305+saitocastel1900@users.noreply.github.com>
2024-07-05 00:16:19 +0900

    ライトの設定を追加

---
+++ Assets/Script/Missile/MissileVisibility.cs
@@ -0,0 +1,7 @@
+namespace Script.Missile
+{
+    public class MissileVisibility
+    {
+        
+    }
+}
---
+++ Assets/Script/Target/IBreakable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public interface IBreakable
+{
+    /// <summary>
+    /// 当てる
+    /// </summary>
+    public void Break(Vector3 position);
+}
