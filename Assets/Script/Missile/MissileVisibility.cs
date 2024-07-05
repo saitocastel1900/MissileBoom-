978125819c73c67043d66be821ad423e44f20af0
Ryoma Saito(Dedicated School Account) <96648305+saitocastel1900@users.noreply.github.com>
2024-07-05 11:44:27 +0900

    不必要な行間、usingを削除

--- Assets/Script/Missile/MissileVisibility.cs
+++ Assets/Script/Missile/MissileVisibility.cs
@@ -1,7 +1,18 @@
-namespace Script.Missile
+using UniRx;
+using UnityEngine;
+
+public class MissileVisibility : MonoBehaviour
 {
-    public class MissileVisibility
+    [SerializeField] private MissileCore _core;
+    [SerializeField] private GameObject _object;
+    
+    void Start()
     {
-        
+        _core
+            .IsBroke
+            .Where(x=>x==true)
+            .Select(x=>x==false)
+            .Subscribe(_object.SetActive)
+            .AddTo(this.gameObject);
     }
 }
