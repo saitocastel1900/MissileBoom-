e7243a53b323d5b83206e5f011211d94b16b0e41
Ryoma Saito(Dedicated School Account) <96648305+saitocastel1900@users.noreply.github.com>
2024-08-13 01:25:35 +0900

    設置場所の変更

--- Assets/Script/Manager/MissileLaunchManager.cs
+++ Assets/Script/Manager/MissileLaunchManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 using UnityEngine;
 using Zenject;
@@ -8,17 +9,18 @@ public class MissileLaunchManager : MonoBehaviour
     [Inject] private AudioManager _audioManager;
     [SerializeField] private MissileCore _missile;
     [SerializeField] private Transform _target;
+    [SerializeField] private float duraton;
     
     void Start()
     {
+        _audioManager.PlayBGM(BgmData.BGM.Battle);
+        
         _input
             .IsButtonPush
             .SkipLatestValueOnSubscribe()
             .FirstOrDefault()
-            .Subscribe(_ =>
-            {
-                _missile.InitializeMissile(_target);
-                _audioManager.PlayBGM(BgmData.BGM.Battle);
-            });
+            .Do(_=>_audioManager.PlaySoundEffect(SoundEffectData.SoundEffect.ButtonClick))
+            .Delay(TimeSpan.FromSeconds(duraton))
+            .Subscribe(_ => _missile.InitializeMissile(_target));
     }
 }
